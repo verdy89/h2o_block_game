b973eb9e5b2028498510e655d7d9f93ec79cf194
verdy89 <verdy.ir.89@gmail.com>
2023-02-10 20:37:30 +0900

    各マスの文字がなんなのかを Board から管理できるようにする

--- src/app/components/board.js
+++ src/app/components/board.js
@@ -7,17 +7,25 @@ export default class Board extends React.Component {
     this.rows = 16;
     this.cols = 10;
     this.charset = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ';
+    this.chars = this.initChar(Array(this.rows * this.cols));
   }
 
   randomAlphabet() {
     return this.charset[Math.floor(Math.random() * this.charset.length)];
   }
 
+  initChar(array) {
+    for (let i = 0; i < array.length; i++) {
+      array[i] = this.randomAlphabet();
+    }
+    return array;
+  }
+
   renderSquare(i) {
     return (
       <Square
         key={ 'square_' + i }
-        value={ this.randomAlphabet() }
+        value={ this.chars[i] }
       />
     )
   }
@@ -27,7 +35,7 @@ export default class Board extends React.Component {
     for (let rowNum = 0; rowNum < this.rows; rowNum++) {
       const row = [];
       for (let colNum = 0; colNum < this.cols; colNum++) {
-        row.push(this.renderSquare(rowNum * this.rows + colNum));
+        row.push(this.renderSquare(rowNum * this.cols + colNum));
       }
       rows.push(<div key={ 'row_' + rowNum } className="board-row">{ row }</div>)
     }
