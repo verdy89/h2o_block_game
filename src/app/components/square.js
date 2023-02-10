de735d10ef93b5ad63222474c89eabfc46ba466e
verdy89 <verdy.ir.89@gmail.com>
2023-02-10 19:45:30 +0900

    クリックしたら背景色が変わるようにする

--- src/app/components/square.js
+++ src/app/components/square.js
@@ -5,20 +5,22 @@ export default class Square extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
-      value: props.value
+      value: props.value,
+      isSelected: false
     };
   }
 
   changeChar() {
     if (['H', 'O'].includes(this.state.value)) {
-      this.setState({ value: '💧' });
+      // this.setState({ value: '💧' });
+      this.setState({ isSelected: true });
     }
   }
 
   render() {
     return (
       <button
-        className={ "square" }
+        className={ "square" + (this.state.isSelected ? ' selected' : '') }
         onClick={ () => this.changeChar() }
       >
         { this.state.value }
